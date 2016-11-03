e7039e8b230be0738418a0a96228004712cbf41f
JpnLavender <nagisa20000014@gmail.com>
2016-11-03 19:46:46 +0900

    インデントがおかしかったので直した

--- app/assets/javascripts/ballot.js
+++ app/assets/javascripts/ballot.js
@@ -1,17 +1,17 @@
-  $(function(){
-    $(".ballot").click(function(){
-      $.ajax({
-        type: 'POST',
-        url: window.location + '/ballot.json',
-        data: {form_ballot: $(this).val()},
-      }).then(function(response){
-        if (response.status === "Successfully") {
-          $(".message").text("投稿が完了しました");
-        } else if (response.status === "Already") {
-          $(".message").text("すでに投稿が完了しています");
-        } else {
-          $(".message").text("エラー");
-        }
-      })
-    });
+$(function(){
+  $(".ballot").click(function(){
+    $.ajax({
+      type: 'POST',
+      url: window.location + '/ballot.json',
+      data: {form_ballot: $(this).val()},
+    }).then(function(response){
+      if (response.status === "Successfully") {
+        $(".message").html("<div class='alert alert-success' role='alert'>投稿が完了しました</div>");
+      } else if (response.status === "Already") {
+        $(".message").html("<div class='alert alert-warning'  role='alert'>すでに投稿を完了しています</div>");
+      } else {
+        $(".message").html("<div class='alert alert-danger' role='alert'>内部サーバーエラー</div>");
+      }
+    })
   });
+});
