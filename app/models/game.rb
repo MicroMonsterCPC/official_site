997ad5ae9813541aab7700b3362b9e028eeedc83
JpnLavender <nagisa20000014@gmail.com>
2016-11-03 09:41:37 +0900

    スコア登録機能追加に伴う関連付けの追加

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ApplicationRecord
   mount_uploader :banner_url, ImageUploader
+  has_many :game_scores
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,3 @@
 class Score < ApplicationRecord
+  has_many :game_scores
 end
