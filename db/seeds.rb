511dd11f3ee914c499b37c0046bb2979b4c4419c
JpnLavender <nagisa20000014@gmail.com>
2016-11-04 17:01:18 +0900

    seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  mail: Rails.application.secrets.user_mail,
+  password: Rails.application.secrets.user_password,
+  password_confirmation: Rails.application.secrets.user_password,
+  administrator: true
+)
+
+Article.create(
+  index: "テスト投稿",
+  about: "テスト投稿"
+)
+
+
