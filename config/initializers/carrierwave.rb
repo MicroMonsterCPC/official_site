35a4dc6aa2d888d1670f27ac0e8cd79b2b105629
JpnLavender <nagisa20000014@gmail.com>
2016-11-04 18:30:13 +0900

    Careerwaveのconfigのバグを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ require 'carrierwave/orm/activerecord'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => Rails.application.secrets,
+    :aws_access_key_id      => Rails.application.secrets.aws_access_key_id,
     :aws_secret_access_key  => Rails.application.secrets.aws_secret_access_key,
     :region                 => 'ap-northeast-1'
   }
